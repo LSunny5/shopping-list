9705ef4f843be0f2c07ab1d3c33f52aa61b83e40
Sunny Lee <LeeSunny507@gmail.com>
2019-12-16 22:50:50 -0500

    added to js add item

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 $(function(){
     //shopping list scripts after return is hit
-    $('#js-shopping-list-form').submit(function(e) {
- 
-        
+    $('#js-shopping-list-form').submit(function(event) {
+        //prevent Default actions
+        event.preventDefault();
+
+        //list entry object created
+        const entry = $('#shopping-list-entry').val();
+
+        //add item
+        $('.shopping-list').append(
+            `<li>
+              <span class="shopping-item">${entry}</span>
+              <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                  <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                  <span class="button-label">delete</span>
+                </button>
+              </div>
+            </li>`);
       
+            //clear the value for next time
+            $('#shopping-list-entry').val('');
     });
 
     //removing the list item
-    $('.shopping-list').on('click', '.shopping-item-delete', function(e){
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event){
         $(this).closest('li').remove();
     });
 
     //checking and unchecking items in list
-    $('.shopping-list').on('click', '.shopping-item-toggle', function(e) {
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
         $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     });
 });
