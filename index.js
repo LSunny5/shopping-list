853368bf23d7c3ba260375f1570e27e2a7a50516
Sunny Lee <LeeSunny507@gmail.com>
2019-12-16 22:16:17 -0500

    checking and unchecking list items js code added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 $(function(){
-    
+    //shopping list scripts after return is hit
+    $('#js-shopping-list-form').submit(function(e) {
+ 
+        
+      
+    });
 
     //removing the list item
     $('.shopping-list').on('click', '.shopping-item-delete', function(e){
         $(this).closest('li').remove();
     });
 
+    //checking and unchecking items in list
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(e) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
 });
 
 
